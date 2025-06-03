11224778903cbefb22b64f59e6c6f170742f78b1
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-03 16:51:08 +0530

    Created a separate route file.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("Hello World");
-}); 
+app.use(express.json());
 
+app.use("/api/products", productRoutes);    
 
 
 app.listen(5000, () => {
