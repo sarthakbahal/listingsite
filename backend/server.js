0f0591569fb650390d394905a6fef4788bf4e382
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-03 19:05:24 +0530

    Initial backend setup completed.

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,16 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);    
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 5000 hello");
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
