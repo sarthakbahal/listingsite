28c3aa72d0dce32ed1b9517fe79a4b3651c96456
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-03 23:26:49 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Box, Button } from "@chakra-ui/react"
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <Box minH={"100vh"} bg={"gray.100"} p={5}>
+      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+
+  )
+}
+
+export default App
