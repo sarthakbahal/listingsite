f7d2ddcdfedf9d1b7db59c1366dcf71807e50790
sarthakbahal <sarthakbahal.45@gmail.com>
2025-06-07 15:41:35 +0530

    Rerun on previous code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000"
-      }
-    }
-  }
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
